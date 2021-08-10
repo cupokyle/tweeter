915576712996ef8112b191e1470c3d3261e1bb21
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-10 16:56:06 +0000

    make char counter turn red when below 0 and back to gray when above

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
   $('#tweet-text').on("input", function() {
     const maxLength = 140;
     const inputLength = $(this).val().length;
-    console.log(inputLength)
+    let $counter = $(this).siblings().children('.counter');
+    $counter.html(maxLength - inputLength);
+    if ($counter.val() < 0){
+      $counter.css("color", "red");
+    } else {
+      $counter.css("color", "");
+    }
   });
 
 });
