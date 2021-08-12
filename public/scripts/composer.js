86106e1f3d3b168c7908f4b2e20728a93f8ed4cb
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-12 19:34:56 +0000

    Add responsive changes for tablet and mobile device

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -29,3 +29,21 @@ $(document).ready(function() {
     $(".arrow").animate({})
   }, 1500);
 });
+
+// Function that automatically grows textarea
+
+$(function() {
+  $('#tweet-text').on('input keyup paste', function() {
+    var $el = $(this),
+        offset = $el.innerHeight() - $el.height();
+
+    if ($el.innerHeight() < this.scrollHeight) {
+      // Grow the field if scroll height is smaller
+      $el.height(this.scrollHeight - offset);
+    } else {
+      // Shrink the field and then re-set it to the scroll height in case it needs to shrink
+      $el.height(1);
+      $el.height(this.scrollHeight - offset);
+    }
+  });
+});
